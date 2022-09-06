64547f2a84aef2239aaba408678c4fab9dd91f83
smartful <remi_rodrigues@hotmail.com>
2022-09-06 04:59:34 +0200

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { Fragment } from "react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Component {...pageProps} />
+      <footer>
+        <p>© 2022 Gandalf</p>
+      </footer>
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
