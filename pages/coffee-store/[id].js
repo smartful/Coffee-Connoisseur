e673abdaaf72a0ad964c7f6c5403311e83dcbcdd
smartful <remi_rodrigues@hotmail.com>
2022-09-10 04:26:41 +0200

    start building coffee store page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,5 +1,9 @@
+import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
+import classnames from "classnames";
+import styles from "../../styles/coffee-store.module.css";
 
 import coffeeStoresData from "../../data/coffee-store.json";
 
@@ -27,15 +31,37 @@ const CoffeeStore = ({ coffeeStore }) => {
   const { id } = router.query;
 
   return (
-    <div>
-      <button>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </button>
-      <div>Coffee Store Page : {id}</div>
-      <p>{coffeeStore.name}</p>
-      <p>{coffeeStore.address}</p>
+    <div className={styles.layout}>
+      <Head>
+        <title>{coffeeStore.name}</title>
+      </Head>
+
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <a>Back to home</a>
+            </Link>
+          </div>
+
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{coffeeStore.name}</h1>
+          </div>
+          <Image
+            src={coffeeStore.imgUrl}
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          />
+        </div>
+
+        <div className={classnames("glass", styles.col2)}>
+          <div className={styles.iconWrapper}></div>
+          <p>{coffeeStore.address}</p>
+          <p>{coffeeStore.neighbourhood}</p>
+        </div>
+      </div>
     </div>
   );
 };
